2d7c3cb4bea18940b0ab917bc3d15fa6ac7f2c5e
Filip <buggfille@gmail.com>
2013-09-17 16:43:45 +0200

    Initial commit

---
+++ include/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<title><?= (trim($title) == false) ? "Default Title" : $title ?></title>
+		<meta charset="utf-8">
+		<meta name="author" content="ada10fl2,ada10jbe" />
+		<meta name="description" content="Webshop" />
+		<script><?= $script ?></script>
+	</head>
+	<body>
+		<div class="topbar">
+			
+		</div>
+		
---
+++ index.php
@@ -0,0 +1,12 @@
+<?
+	$script = "";
+	$title = "";
+	include $_SERVER["DOCUMENT_ROOT"]."/include/header.php";
+?>
+
+<h1>This is index.php</h1>
+
+<?
+	include $_SERVER["DOCUMENT_ROOT"].'/include/footer.php';
+?>
+
