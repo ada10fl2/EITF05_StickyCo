89a8f205aace98be6f9431841499df6e32e04031
Filip Lindqvist <ada10fl2@student.lu.se>
2013-09-22 13:45:55 +0200

    Changed to http in fillprods.php

--- fillprods.php
+++ fillprods.php
@@ -7,7 +7,7 @@ $imgs = scandir($_SERVER['DOCUMENT_ROOT']."/img/small");
   	$p = str_replace ( ".png" , "" , $l);
   	$p = str_replace ( "_" , " " , $p);
   	$price = rand ( 1 , 20 );
-  	$text = json_decode(file_get_contents("https://baconipsum.com/api/?type=all-meat&paras=1&start-with-lorem=0&sentences=3"))[0];
+  	$text = json_decode(file_get_contents("http://baconipsum.com/api/?type=all-meat&paras=1&start-with-lorem=0&sentences=3"))[0];
    	$db->create_prod($l,ucwords($p), $price, $text);
    }
  }
